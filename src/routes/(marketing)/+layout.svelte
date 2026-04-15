e97c61b35548f2d6f81c558e25752c2cb8a36869
M. Aulia <muhamadauliarahmannasution@gmail.com>
2026-04-15 17:45:25 +0700

    Group Routes marketing

---
+++ src/routes/(marketing)/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import '../layout.css';
+	import Logo from '../../../static/LogoScribtware.avif';
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={Logo} /></svelte:head>
+{@render children()}
---
+++ src/routes/(marketing)/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import {
+		ClassMenu,
+		CTA,
+		FAQ,
+		Herosection,
+		Mentors,
+		Personalisation,
+		Studysteps,
+		Testimonials,
+		USP
+	} from '$lib/components/landingpage';
+</script>
+
+<main class="font-sans text-gray-800">
+	<Herosection />
+	<USP />
+	<Personalisation />
+	<ClassMenu />
+	<Studysteps />
+	<Mentors />
+	<Testimonials />
+	<FAQ />
+	<CTA />
+</main>
