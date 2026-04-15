e7f5becd9ec47309c326a6f683e3ddf812a3ecff
M. Aulia <muhamadauliarahmannasution@gmail.com>
2026-04-15 17:44:06 +0700

    deleting old dir

--- src/routes/contactus/components/Footer.svelte
+++
@@ -1,6 +0,0 @@
-<footer class="mt-14 flex flex-col items-center gap-1 border-t border-gray-200 py-7">
-	<span class="text-[16px] tracking-widest text-gray-300 select-none"> 我们期待您的来信 </span>
-	<span class="text-[10.5px] tracking-[0.16em] text-gray-400 italic">
-		Kami menantikan kabar dari Anda
-	</span>
-</footer>
